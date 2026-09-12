80b11cdbbaa9eac4681e97a8d24d00c7b838cdee
jaimecardona-lgtm <jaimeandres10010@gmail.com>
2026-09-12 01:06:28 -0500

    chore(email): remove obsolete duplicate email service

--- src/integrations/email/email.service.ts
+++
@@ -1,8 +0,0 @@
-import type { EmailMessage, EmailSendResult } from "./email-provider";
-import { GmailProvider } from "./gmail.provider";
-
-const provider = new GmailProvider();
-
-export async function sendEmail(message: EmailMessage): Promise<EmailSendResult> {
-  return provider.send(message);
-}
